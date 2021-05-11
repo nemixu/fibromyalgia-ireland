b4a7e26c04bcf3f3ffd39dbbafee1d584249c93a
nemixu <s.r.seagrave@gmail.com>
2021-05-11 23:17:07 +0100

    git add route

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('/', views.index, name='home'),
     path('contact/', views.contact, name='contact'),
     path('contact/success/', views.contact_success, name='contact_success'),
 ]
